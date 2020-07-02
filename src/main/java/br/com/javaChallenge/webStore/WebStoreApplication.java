b39e9493b1bfcbf2991fa3b6e7b7ecbf6e0bccb1
Lucas Felipe Alves <lucas.alves@quantus.com.br>
2020-07-02 11:58:08 -0300

    Inicio de projeto.

---
+++ src/main/java/br/com/javaChallenge/webStore/WebStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.javaChallenge.webStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/javaChallenge/webStore/WebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javaChallenge.webStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
